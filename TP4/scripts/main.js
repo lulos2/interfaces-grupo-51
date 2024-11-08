072e30f138a430aa598e8beba4d19ba560f72a11
lulo <lulobareiss@gmail.com>
2024-11-08 13:12:32 -0300

    feat: implement carousel and add figures 4 and 5

--- TP4/scripts/main.js
+++ TP4/scripts/main.js
@@ -16,3 +16,24 @@ function handleScroll() {
 }
 
 window.addEventListener('scroll', handleScroll);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const carouselContainer = document.querySelector('.carousel');
+    const images = [
+        '../TP4/assets/images/carousel/default.png',
+        '../TP4/assets/images/carousel/img.png',
+        '../TP4/assets/images/carousel/img_1.png',
+        '../TP4/assets/images/carousel/img_2.png'
+    ];
+    let currentIndex = 0;
+    const interval = 3000; // Cambiar cada 3 segundos
+
+    function loadImage(index) {
+        carouselContainer.style.backgroundImage = `url('${images[index]}')`;
+    }
+    // llama a la funcion cada 3 segundos
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+        loadImage(currentIndex);
+    }, interval);
+});
