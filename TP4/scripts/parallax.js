ffcd58f166f18e4d22b7dbddc662bccd14a1f0ad
lulo <lulobareiss@gmail.com>
2024-11-15 12:41:34 -0300

    feat: add parallax effect to section "la app mas divertida"

--- TP4/scripts/parallax.js
+++ TP4/scripts/parallax.js
@@ -1,5 +1,5 @@
 
-class ObserverParallax {
+class ElementParallax {
     constructor(element, parallaxFactor) {
         this.element = element;
         this.parallaxFactor = parallaxFactor;
@@ -32,15 +32,8 @@ class ObserverParallax {
         });
     }
 
-    applyParallaxInTop(scrollTop) {
-        this.element.style.transform = `translateY(${scrollTop * this.parallaxFactor}px)`;
-    }
-
-    applyParallaxEffect(scrollTop) {
-        if (this.isVisible && this.scrollStart !== null) {
-            const scrollDiff = scrollTop - this.scrollStart;
-            this.element.style.transform = `translateY(${scrollDiff * this.parallaxFactor}px)`;
-        }
+    applyParallaxWithCorrection(scrollTop, pxCorrection) {
+        this.element.style.transform = `translateY(${(scrollTop - pxCorrection) * this.parallaxFactor}px)`;
     }
 
     applyParallaxEfectMouse(mousePositionX, mousePositionY) {
@@ -52,28 +45,32 @@ class ObserverParallax {
         }
     }
 
-    resetPosition() {
-        this.element.style.transform = '';
-    }
 }
 
-let observerTreeLeft = new ObserverParallax(document.querySelector('.tree-left'), -0.05);
-let observerTreeRight1 = new ObserverParallax(document.querySelector('.tree-right-1'), -0.05);
-let observerTreeRight2 = new ObserverParallax(document.querySelector('.tree-right-2'), -0.05);
-let observerStoneLeft = new ObserverParallax(document.querySelector('.stone-left'), -0.07);
-let observerStoneRight1 = new ObserverParallax(document.querySelector('.stone-right-1'), -0.05);
-let observerStoneRight2 = new ObserverParallax(document.querySelector('.stone-right-2'), -0.09);
-let observerStoneRight3 = new ObserverParallax(document.querySelector('.stone-right-3'), -0.1);
-let observerBushLeft1 = new ObserverParallax(document.querySelector('.bush-left-1'), -0.04);
-let observerBushLeft2 = new ObserverParallax(document.querySelector('.bush-left-2'), -0.13);
-let observerBushRight1 = new ObserverParallax(document.querySelector('.bush-right-1'), -0.05);
-let observerBushRight2 = new ObserverParallax(document.querySelector('.bush-right-2'), -0.1);
-let observerFigure1 = new ObserverParallax(document.querySelector('.figure-1'), -0.09);
-let observerFigure2 = new ObserverParallax(document.querySelector('.figure-2'), -0.12);
-let observerFigure3 = new ObserverParallax(document.querySelector('.figure-3'), -0.1);
-let observerSection2Figure3 = new ObserverParallax(document.querySelector('.section-2 .figure-3'), -0.1);
+let observerTreeLeft = new ElementParallax(document.querySelector('.tree-left'), -0.05);
+let observerTreeRight1 = new ElementParallax(document.querySelector('.tree-right-1'), -0.05);
+let observerTreeRight2 = new ElementParallax(document.querySelector('.tree-right-2'), -0.05);
+let observerStoneLeft = new ElementParallax(document.querySelector('.stone-left'), -0.07);
+let observerStoneRight1 = new ElementParallax(document.querySelector('.stone-right-1'), -0.05);
+let observerStoneRight2 = new ElementParallax(document.querySelector('.stone-right-2'), -0.09);
+let observerStoneRight3 = new ElementParallax(document.querySelector('.stone-right-3'), -0.1);
+let observerBushLeft1 = new ElementParallax(document.querySelector('.bush-left-1'), -0.04);
+let observerBushLeft2 = new ElementParallax(document.querySelector('.bush-left-2'), -0.13);
+let observerBushRight1 = new ElementParallax(document.querySelector('.bush-right-1'), -0.05);
+let observerBushRight2 = new ElementParallax(document.querySelector('.bush-right-2'), -0.1);
+let observerSection1Figure1 = new ElementParallax(document.querySelector('.figure-1'), -0.17);
+let observerSection1Figure2 = new ElementParallax(document.querySelector('.figure-2'), -0.22);
+let observerSection1Figure3 = new ElementParallax(document.querySelector('.figure-3'), -0.2);
+let observerSection1Figures = new ElementParallax(document.querySelector('.section-1 .figures-123459'), 0.1);
 
-let observerSection1Figures = new ObserverParallax(document.querySelector('.section-1 .figures-123459'), 0.1);
+let laAppMasDivertidaH1 = new ElementParallax(document.querySelector('.la-app-mas-divertida'), -0.1);
+let laAppMasDivertidaDesc = new ElementParallax(document.querySelector('.la-app-mas-divertida-desc'), -0.1);
+let section1Figure5 = new ElementParallax(document.querySelector('.section-1 .figure-5'), 0.05);
+let section1Figure4 = new ElementParallax(document.querySelector('.section-1 .figure-4'), -0.01);
+let section1Carousel = new ElementParallax(document.querySelector('.section-1 .carousel'), 0.02);
+
+let observerSection2Figure3 = new ElementParallax(document.querySelector('.section-2 .figure-3'), -0.6);
+
 
 document.addEventListener('mousemove', (e) => {
     let mousePositionX = e.clientX ;
@@ -83,20 +80,26 @@ document.addEventListener('mousemove', (e) => {
 
 document.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset;
-    observerTreeLeft.applyParallaxInTop(scrollTop);
-    observerTreeRight2.applyParallaxInTop(scrollTop);
-    observerTreeRight1.applyParallaxInTop(scrollTop);
-    observerBushRight2.applyParallaxInTop(scrollTop);
-    observerBushRight1.applyParallaxInTop(scrollTop);
-    observerBushLeft2.applyParallaxInTop(scrollTop);
-    observerBushLeft1.applyParallaxInTop(scrollTop);
-    observerStoneRight3.applyParallaxInTop(scrollTop);
-    observerStoneRight2.applyParallaxInTop(scrollTop);
-    observerStoneRight1.applyParallaxInTop(scrollTop);
-    observerStoneLeft.applyParallaxInTop(scrollTop);
-    observerFigure1.applyParallaxInTop(scrollTop);
-    observerFigure2.applyParallaxInTop(scrollTop);
-    observerFigure3.applyParallaxInTop(scrollTop);
+    observerTreeLeft.applyParallaxWithCorrection(scrollTop,0);
+    observerTreeRight2.applyParallaxWithCorrection(scrollTop,0);
+    observerTreeRight1.applyParallaxWithCorrection(scrollTop,0);
+    observerBushRight2.applyParallaxWithCorrection(scrollTop,0);
+    observerBushRight1.applyParallaxWithCorrection(scrollTop,0);
+    observerBushLeft2.applyParallaxWithCorrection(scrollTop,0);
+    observerBushLeft1.applyParallaxWithCorrection(scrollTop,0);
+    observerStoneRight3.applyParallaxWithCorrection(scrollTop,0);
+    observerStoneRight2.applyParallaxWithCorrection(scrollTop,0);
+    observerStoneRight1.applyParallaxWithCorrection(scrollTop,0);
+    observerStoneLeft.applyParallaxWithCorrection(scrollTop,0);
+    observerSection1Figure1.applyParallaxWithCorrection(scrollTop,0);
+    observerSection1Figure2.applyParallaxWithCorrection(scrollTop,0);
+    observerSection1Figure3.applyParallaxWithCorrection(scrollTop,0);
 
-    observerSection2Figure3.applyParallaxEffect(scrollTop);
+    laAppMasDivertidaH1.applyParallaxWithCorrection(scrollTop, 500);
+    laAppMasDivertidaDesc.applyParallaxWithCorrection(scrollTop, 530);
+    section1Figure4.applyParallaxWithCorrection(scrollTop, 700);
+    section1Figure5.applyParallaxWithCorrection(scrollTop, 800);
+    section1Carousel.applyParallaxWithCorrection(scrollTop, 1000);
+
+    observerSection2Figure3.applyParallaxWithCorrection(scrollTop,12000);
 });
